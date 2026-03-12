1b762fea1bfd1e96daa7f5f037cc082782808659
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-12 12:41:04 +0530

    used useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [now, setNow] = useState(()=> new Date())
+
+  useEffect(()=>{
+    let timerId = setInterval(() => {
+      setNow(new Date()); 
+    }, 1000);
+
+    return ()=> clearInterval(timerId)
+  }, [])
+
+  let dateOptions = {
+    day : "numeric",
+    month : "long",
+    year : "numeric"
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-full bg-slate-100 flex items-center justify-center p-24px">
+      <div className="max-w-[550px] border border-slate-500 p-4 text-center rounded-xl">
+        <h1 className="text-4xl mb-4">Digital Clock</h1>
+
+        <div>
+          <p className="text-8xl mb-2">{now.getHours()} : {now.getMinutes()} : {String(now.getSeconds()).padStart(2, "0")}</p>
+          <p className="text-4xl text-gray-700">{now.toLocaleDateString("en-GB", dateOptions)}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
